4df8f444b7810548b678f2d2d920fe808a10acd0
Nitin Kukreja <nitkuk1992@gmail.com>
2019-10-03 16:07:11 +0530

    Adds create, edit and remove polygon start and end hooks

--- src/helpers/PubSub.js
+++ src/helpers/PubSub.js
@@ -1,26 +1,27 @@
 const PubSub = () => {
+  const state = {};
 
-    const state = {};
+  const publish = async (evtName, data) => {
+    const promises = state[evtName].map(cb => {
+      return Promise.resolve(cb(data));
+    });
+    const v = await Promise.all(promises);
+    return v[0];
+  };
 
-    const publish = (evtName, data) => {
-        state[evtName].forEach(cb => {
-            cb(data);
-        });
+  const subscribe = (evtName, callback) => {
+    state[evtName] = state[evtName] || [];
+    state[evtName].push(callback);
+
+    return () => {
+      state[evtName] = state[evtName].filter(c => c !== callback);
     };
+  };
 
-    const subscribe = (evtName, callback) => {
-        state[evtName] = state[evtName] || [];
-        state[evtName].push(callback);
-
-        return () => {
-            state[evtName] = state[evtName].filter(c => c !== callback);
-        };
-    };
-
-    return {
-        publish,
-        subscribe
-    };
+  return {
+    publish,
+    subscribe
+  };
 };
 
 export const pubSub = PubSub();
